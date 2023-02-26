2184705dcfb652e45f36d66a7ba782b643ecd406
Ye Joo Park <subwaymatch@gmail.com>
2023-02-26 04:50:54 -0600

    ObjectCreated trigger without docker

---
+++ handler.py
@@ -0,0 +1,25 @@
+import json
+import boto3
+import os
+
+def hello(event, context):
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    print(body)
+
+    return {"statusCode": 200, "body": json.dumps(body)}
+
+def handleS3Upload(event, context):
+    bucket = os.environ['JUPYTER_NOTEBOOK_SUBMISSION_BUCKET']
+    region_name = os.environ['REGION_NAME']
+
+    filesUploaded = event['Records']
+
+    for file in filesUploaded:
+        fileName = file["s3"]["object"]["key"]
+        print(fileName)
+
+    return(fileName)
